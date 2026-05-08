4216ba620769c69258b66efd710bcd69028d047d
suctiontomb <fanieduminy7@gmail.com>
2026-05-08 18:26:05 +0300

    Add filtering to API endpoints

--- tools/views.py
+++ tools/views.py
@@ -3,12 +3,17 @@ from tools.serializers import ToolSerializer,TechnicianSerializer, ToolCheckoutS
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 class ToolViewSet(viewsets.ModelViewSet):
     queryset = Tool.objects.all()
     serializer_class = ToolSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filterset_fields = ['condition']
+    search_fields = ['name', 'serial_number']
 
 
 class TechnicianViewSet(viewsets.ModelViewSet):
@@ -16,12 +21,15 @@ class TechnicianViewSet(viewsets.ModelViewSet):
     serializer_class = TechnicianSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend,SearchFilter]
+    search_fields = ['name']
 
 class ToolCheckoutViewSet(viewsets.ModelViewSet):
     queryset = ToolCheckout.objects.all()
     serializer_class = ToolSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-    
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['notes']
 
 
