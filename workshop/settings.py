6bf37be7deff71d88fa4d58d92679b045e55c282
suctiontomb <fanieduminy7@gmail.com>
2026-05-08 14:04:54 +0300

    Migrate from SQLite to PostgreSQL

--- workshop/settings.py
+++ workshop/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'workshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'workshop',
+        'USER': 'workshop_user',
+        'PASSWORD': 'workshop123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
